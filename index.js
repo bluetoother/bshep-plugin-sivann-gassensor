62316833d31f5fdfd4d2507f9cd03d28501a2567
Hedy Wang <hedywings@gmail.com>
2016-06-15 21:47:59 +0800

    v0.0.2

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 module.exports = {
     gattDefs: {
         service: [
-            { name: 'dinServ', uuid: '0xbb00' },
-            { name: 'ainServ', uuid: '0xbb10' },
             { name: 'environServ', uuid: '0xbb50' },
             { name: 'buzzerServ', uuid: '0xbb60' }
         ],
         characteristic: [
-            { name: 'ainMeasPeriod', uuid: '0xbb11', params: ['period'], types: ['uint8'] },
             { name: 'gasMeasPeriod', uuid: '0xbb51', params: ['period'], types: ['uint8'] }
         ]
      },
