3178cd10f847aa54f4a2d263b6e8198509b7fc44
Hedy Wang <hedywings@gmail.com>
2016-09-02 15:20:53 +0800

    v0.1.0

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 module.exports = {
     gattDefs: {
         service: [
+            { name: 'dInServ', uuid: '0xbb00' },
+            { name: 'aInServ', uuid: '0xbb10' },
             { name: 'environServ', uuid: '0xbb50' },
             { name: 'buzzerServ', uuid: '0xbb60' }
         ],
         characteristic: [
-            { name: 'gasMeasPeriod', uuid: '0xbb51', params: ['period'], types: ['uint8'] }
+            { name: 'aInConfig', uuid: '0xbb11', params: ['config'], types: ['boolean'] },
+            { name: 'aInMeasPeriod', uuid: '0xbb12', params: ['period'], types: ['uint8'] },
+            { name: 'gasConfig', uuid: '0xbb51', params: ['config'], types: ['boolean'] },
+            { name: 'gasMeasPeriod', uuid: '0xbb52', params: ['period'], types: ['uint8'] }
         ]
      },
      examine: function (periph, basicInfo) {
         var isMine = false;
 
         if (basicInfo.manufacturer === 'sivann' &&
-            basicInfo.devName === 'Gas Sht Sensor' &&
-            basicInfo.model === 'GasShtSensor' &&
+            basicInfo.devName === 'Gas Alarm Sensor' &&
+            basicInfo.model === 'GasAlarmSensor' &&
             basicInfo.fwRev === 'v1.0.0' && 
             basicInfo.hwRev === 'v1.0.0' &&
             basicInfo.swRev === 'v1.0.0')
